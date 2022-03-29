fddbcea0cb9cfc9f867917cbef76c260cfdfbbd1
mjp0 <marko@machian.com>
2022-03-29 15:42:01 +0200

    removed unused files

--- protocols/backbone_index_writequeue/index.ts
+++
@@ -1,3 +0,0 @@
-import Protocol from './protocol'
-import API from './API'
-export default { Protocol, API }
--- protocols/index.ts
+++ protocols/index.ts
@@ -1,13 +1,9 @@
 import chat from './chat'
 import keyvalue from './keyvalue'
-import index from './backbone_index'
-import writequeue from './backbone_index_writequeue'
 import tweeterdemo from './tweeterdemo'
 
 export default { 
   chat,
   keyvalue,
-  index,
-  writequeue,
   tweeterdemo
 }

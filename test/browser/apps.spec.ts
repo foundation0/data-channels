c720ad3be70137bc8ea8cf941ece5eca7ab5f008
mjp0 <marko@machian.com>
2022-05-19 14:52:19 +0000

    wip

--- test/browser/apps.spec.ts
+++ test/browser/apps.spec.ts
@@ -4,12 +4,13 @@ import { CoreConfig } from '../../common/interfaces'
 import { cleanup } from '../helper'
 import default_config from '../../bbconfig'
 import Apps from '../../apps'
+// import { createStore } from 'idb-keyval'
 
 const config_kv: CoreConfig = {
   address: 'protocol-kv',
   encryption_key: default_config.keys.index,
   writers: [],
-  indexes: [],
+  trusted_peers: [],
   private: true,
   storage: 'ram',
   storage_prefix: 'test',
@@ -19,6 +20,8 @@ test.beforeEach(cleanup)
 test.afterEach(cleanup)
 
 test('Apps', async () => {
+  // const idsStore = createStore("ids", "backbone")
+  expect(window).toBeTruthy()
   const core = await Core({ config: config_kv, app: Apps['keyvalue'] })
 
   await core.set({ key: 'key', value: 'value' })

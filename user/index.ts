f5f7c75087903b35c97e325c39cf2ef26b387c2e
mjp0 <marko@machian.com>
2022-05-25 23:43:00 +0000

    wip

--- user/index.ts
+++ user/index.ts
@@ -1,141 +1,72 @@
-import { getRandomInt, registerMethods } from '../common'
-import { securePassword } from '../common/crypto'
-import { CoreConfig, UserConfig } from '../common/interfaces'
-import { ethers } from 'ethers'
-import KeyVault from './keyvault'
-import Buffer from 'b4a'
-import { createHash, discoveryKey, buf2hex, encrypt, decrypt, hex2buf } from '@backbonedao/crypto'
-import { HDNode } from 'ethers/lib/utils'
-import Auth from '../auth'
-class UserClass {
-  private keyvault
-  private user_config
-  username: string
-  wallet: HDNode
-  network_id: { secretKey: string; publicKey: string }
-  status: string
-  signAction: Function
-  getPublicKey: Function
-  addPath: Function
-  getPath: Function
-  getSeed: Function
-  generateNewPath: Function
-  getUsername: Function
-  getWallet: Function
+import * as Comlink from 'comlink'
+import Config from '../bbconfig'
+import { registerMethods } from '../common'
 
-  constructor(user_config: UserConfig) {
-    this.user_config = user_config
-    this.username = user_config.username
+class UserManagerClass {
+  AuthApp: { isAuthenticated: Function } | null = null
 
-    this.init = this.init.bind(this)
-    this.getUserObjAddress = this.getUserObjAddress.bind(this)
-    this.createUserObject = this.createUserObject.bind(this)
-    this.openUserObject = this.openUserObject.bind(this)
-    this.authenticate = this.authenticate.bind(this)
-    this.getNetworkId = this.getNetworkId.bind(this)
-    this.createNewCore = this.createNewCore.bind(this)
-  }
-  async init(this: UserClass, core_config_override?) {
-    // if type is native, create signer based on keyvault
-    // if type is ledger, try to connect to ledger
+  constructor() {}
 
-    // this.keyvault = await KeyVault(this.user_config, core_config_override)
-    // if (this.keyvault.status !== 'active') throw new Error('KEYVAULT INITIALIZATION ERROR')
+  async init(this: UserManagerClass) {
+    const self = this
+    let auth_app_e = document.getElementById('bb-auth-app')
 
-    // this.addPath = this.keyvault.addPath
-    // this.getPath = this.keyvault.getPath
-    // this.status = this.keyvault.status
-    this.getSeed = () => this.keyvault.getSeed(this.user_config.password)
-    this.getUsername = () => this.username
-    this.generateNewPath = async () => {
-      // here would be a great place to implement a timelock for password usage
-      return this.keyvault.generateNewPath(this.user_config.password)
+    // if auth app hasn't been initialized, init it
+    if (!auth_app_e) {
+      auth_app_e = document.createElement('iframe')
+      auth_app_e.style.display = 'none'
+      auth_app_e.setAttribute('id', 'bb-auth-app')
+      auth_app_e.setAttribute('src', Config.user.auth_app_url)
+      document.body.appendChild(auth_app_e)
     }
+
+    // establish connection to auth app
+    const ifr = document.getElementById('bb-auth-app')
+    if (ifr) {
+      await new Promise((resolve) => (ifr.onload = resolve))
+      // @ts-ignore - ts doesn't recognize ifr is iframe
+      self.AuthApp = Comlink.wrap(Comlink.windowEndpoint(ifr.contentWindow))
+      // @ts-ignore - ts doesn't work well with Comlink
+      const pong = await self.AuthApp.ping()
+      if (!pong) throw new Error(`communication with auth app failed`)
+    } else throw new Error(`couldn't initialize auth app`)
   }
 
-  getUserObjAddress(this: UserClass, params: { password: string }) {
-    return buf2hex(
-      discoveryKey(
-        createHash(
-          securePassword({
-            password: Buffer.from(params.password),
-            salt: Buffer.from(this.username),
-          })
-        )
-      )
+  async authenticate(this: UserManagerClass) {
+    const popup = window.open(
+      Config.user.auth_app_url,
+      'auth-app',
+      'width=500,height=300'
     )
+    if (popup) {
+      return new Promise((resolve, reject) => {
+        function authenticated(is_authenticated) {
+          if (is_authenticated) {
+            console.log('authenticated')
+            resolve(true)
+          } else reject(`couldn't authenticate`)
+        }
+        Comlink.expose(authenticated, Comlink.windowEndpoint(popup))
+      })
+    } else throw new Error(`couldn't open auth app`)
   }
 
-  createUserObject(
-    this: UserClass,
-    params: { root_pass: string; password: string; reminder: string }
-  ) {
-    const user_obj = {
-      reminder: params.reminder,
-      enc: encrypt({ key: params.password, data: params.root_pass }),
-    }
-    return user_obj
-  }
-
-  openUserObject(this: UserClass, params: { enc: object; password: string }) {
-    return decrypt({ key: params.password, ...params.enc })
-  }
-
-  authenticate(this: UserClass, params: { signature: string }) {
-    this.wallet = ethers.utils.HDNode.fromSeed(createHash(params.signature))
-    const netid = this.wallet.derivePath(`m/44'/60'/0'/0/0`)
-    this.network_id = {
-      secretKey: netid.privateKey.replace(/0x/, ''),
-      publicKey: netid.publicKey.replace(/0x/, ''),
-    }
-    // todo: open keyvault
-  }
-
-  getNetworkId(this: UserClass, params: { seed: string }) {
-    if (!this.wallet) throw new Error('NOT AUTHENTICATED')
-    return this.network_id
-  }
-
-  createNewCore(this: UserClass, params: { path?: string; name: string; core?: CoreConfig }) {
-    if (!params.name) throw new Error('CORE NEEDS NAME')
-    const max_path = 2140000000
-    const level1 = params?.path?.split('/')[0] || getRandomInt(0, max_path)
-    const level2 = params?.path?.split('/')[1] || getRandomInt(0, max_path)
-    const path = `m/44'/60'/0'/${level1}/${level2}`
-    const dpath = this.wallet.derivePath(path)
-    const core_meta = {
-      secretKey: dpath.privateKey.replace(/0x/, ''),
-      publicKey: dpath.publicKey.replace(/0x/, ''),
-      path,
-      name: params.name
-    }
-    if(!params.core) return core_meta
-
-    // create core
-
-    // save path to keyvault
+  async isAuthenticated(this: UserManagerClass) {
+    const self = this
+    if (!self.AuthApp) await self.init()
+    if (self.AuthApp) return self.AuthApp.isAuthenticated()
+    else throw new Error(`no auth app available`)
   }
 }
 
-async function User(user_config?: UserConfig, core_config_override?) {
-  if(!user_config) return Auth
-  const U = new UserClass(user_config)
-  await U.init(core_config_override)
-  const API = registerMethods({
-    source: U,
-    methods: [
-      'status',
-      'getUsername',
-      'getWallet',
-      'getUserObjAddress',
-      'createUserObject',
-      'openUserObject',
-      'authenticate',
-      'getNetworkId',
-      'createNewCore'
-    ],
-  })
-  return API
+async function UserManager() {
+  const AM = new UserManagerClass()
+  await AM.init()
+  // const API = registerMethods({
+  //   source: AM,
+  //   methods: ['authenticate', 'isAuthenticated', 'init'],
+  // })
+  return AM
 }
 
-export default User
+export default UserManager
